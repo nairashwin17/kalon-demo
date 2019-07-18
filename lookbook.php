98a511d8d7a76756e4d5b4b48a8fa2fa21ef9abc
Ashwin Nair <ashwin.nair@sts.in>
2019-07-18 11:49:34 +0530

    lookbok page design

---
+++ lookbook.php
@@ -0,0 +1,40 @@
+<?php  include 'header.php'; ?>
+    <div class="homepage">
+        <div class="headerwrapper">
+            <?php include 'topbar.php';?>
+            <?php include 'navbar.php';?>
+        </div>
+        <div class="lookbook-wrapper topspacer">
+            <div class="container">
+                <div class="thumbnail-lookbook text-center">  
+                    <div class="row">
+                            <div class="col-md-3 lookbook-box">
+                                <img src="img/col5.png" alt="">  
+                            </div>
+                            <div class="col-md-3 lookbook-box">
+                                <img src="img/col6.png" alt="">  
+                            </div>
+                            <div class="col-md-3 lookbook-box">
+                                <img src="img/col7.png" alt="">  
+                            </div>
+                            <div class="col-md-3 lookbook-box">
+                                <img src="img/col8.png" alt="">  
+                            </div>
+                            <div class="col-md-3 lookbook-box">
+                                <img src="img/col9.png" alt="">
+                            </div>
+                            <div class="col-md-3 lookbook-box">
+                                <img src="img/col10.png" alt="">  
+                            </div>
+                            <div class="col-md-3 lookbook-box">
+                                <img src="img/col11.png" alt="">   
+                            </div>
+                            <div class="col-md-3 lookbook-box">
+                                <img src="img/col12.png" alt="">  
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php  include 'footer.php'; ?>
