c52a6e501781a187b733bed90b8351274ff13b6f
nairashwin17 <nairashwin17@gmail.com>
2019-07-20 12:51:25 +0530

    login page

---
+++ latest_news.php
@@ -0,0 +1,31 @@
+<?php  include 'header.php'; ?>
+    <div class="homepage">
+        <div class="headerwrapper">
+            <?php include 'topbar.php';?>
+            <?php include 'navbar.php';?>
+        </div>
+        <div class="latestnews topspacer">
+                <div class="container">
+                    <div class="row no-pd">
+                            <div class="col-md-6 p8530">
+                                <h1>Priyanka Chopra</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                <button class="kalon-btn-blue">Read more</button>
+                            </div>
+                            <div class="col-md-6">
+                                <img src="img/Artboard15.png" alt="">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="img/Artboard16.png" alt="">
+                            </div>
+                            <div class="col-md-6 p8530">
+                            <h1>Yami Gautam </h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                <button class="kalon-btn-blue">Read more</button>  
+                            </div>
+                    </div> 
+                </div>
+         </div>    
+    </div>  
+
+<?php  include 'footer.php'; ?>
---
+++ login.php
@@ -0,0 +1,16 @@
+<?php  include 'header.php'; ?>
+<div class="homepage">
+    <div class="headerwrapper">
+        <?php include 'topbar.php';?>
+        <?php include 'navbar.php';?>
+    </div>
+    <div class="login-wrapper topspacer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5">hi</div>
+                <div class="col-sm-7">bye</div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php  include 'footer.php'; ?>
