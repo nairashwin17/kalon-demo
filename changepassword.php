85c78919c88077e6dc77f88584345c25b0cab435
Ashwin Nair <ashwin.nair@sts.in>
2019-07-29 11:25:02 +0530

    changepassword & useradminpage layout structure

---
+++ changepassword.php
@@ -0,0 +1,27 @@
+<?php  include 'header.php'; ?>
+<div class="homepage">
+    <div class="headerwrapper">
+        <?php include 'topbar.php';?>
+        <?php include 'navbar.php';?>
+    </div>
+    <div class="changepassword">
+        <div class="changepassword-wrapper topspacer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3 ">
+                       <div class="col-md-12 profilepic">Profile pic</div>
+                       <br>
+                       <div class="col-md-12 profiledetails">Profile Details</div>
+                    </div>
+                    <div class="col-sm-9 userdata">
+                        <div class="col-md-12">
+                            change password form
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
+<?php  include 'footer.php'; ?>
---
+++ useradminpage.php
@@ -0,0 +1,27 @@
+<?php  include 'header.php'; ?>
+<div class="homepage">
+    <div class="headerwrapper">
+        <?php include 'topbar.php';?>
+        <?php include 'navbar.php';?>
+    </div>
+    <div class="changepassword">
+        <div class="changepassword-wrapper topspacer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3 ">
+                       <div class="col-md-12 profilepic">User pic</div>
+                       <br>
+                       <div class="col-md-12 profiledetails">User Details</div>
+                    </div>
+                    <div class="col-sm-9 profiledata">
+                        <div class="col-md-12">
+                            All User Details
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
+<?php  include 'footer.php'; ?>
