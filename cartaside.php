c7fc02f4c0de42bbfc7fbec651c12f5ed8362798
Ashwin Nair <ashwin.nair@sts.in>
2019-07-29 17:39:20 +0530

    cart pages

---
+++ cartaside.php
@@ -0,0 +1,14 @@
+<?php  include 'header.php'; ?>
+<div class="homepage">
+    <div class="headerwrapper">
+        <?php include 'topbar.php';?>
+        <?php include 'navbar.php';?>
+    </div>
+    <div class="cartwrapper topspacer">
+        <div class="fullwidth">
+            <div class="cartproduct">sdasd</div>
+            <div class="cartright">x</div>
+        </div>
+    </div>
+</div>
+<?php  include 'footer.php'; ?>
